4a14de784f56b7b4a03e73ddd07b620a12c6fdd0
Delisma <delispence7@gmail.com>
2024-02-08 01:18:46 -0500

    added

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 100 ) + 1
+
+const num3 = 10;
+const num4 = 6;
+
+const mod = num3 - num4;
+
+const set = [10, 5, 20, 15, 3];
+const max = Math.max(...set);
+
+console.log(max);
+//console.log(mod);
+//console.log(remainder);
+// function random() {
+//   return 1
+//   // return Math.floor(Math.random() * 100 ) + 1
+// }
+//const randomNumber = getRandomNumber();
+//console.log(randomNumber);
+
+
